7486e20d47a1b65f39a88bb395029e71970d466c
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-06-23 00:53:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import search from './search'
 
 function App() {
   const handleClick = () => {
-    const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://www.google.com/search?q=site%3Amenshealth.com/style+${randSearch}&sca_esv=44f1fb0a97cf7ff0&sca_upv=1&ei=a88bZqmBOJGE4-EPgYKxiA4&ved=0ahUKEwjp4oHO3cGFAxURwjgGHQFBDOEQ4dUDCBA&uact=5&oq=site%3Amenshealth.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWVuc2hlYWx0aC5jb20gYUiqjAhQAFgAcAR4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
+    window.location = `https://www.reddit.com/r/randnsfw/`
   }
   return (
     <div className='App' onClick={handleClick}>
