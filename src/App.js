6a902004c72391cef4edeabd6c32d067d4b3b122
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-11-09 02:31:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import search from './search'
 
 function App() {
   const handleClick = () => {
-    const randSearch = search[Math.floor(Math.random() * (7745 - 1 + 1) + 1 )]
+    const randSearch = search[Math.floor(Math.random() * (2995 - 1 + 1) + 1 )]
     
-    window.location = `https://redditfavorites.com/?q=${randSearch}`
+    window.location = `https://www.google.com/search?q=site%3Agoodreads.com+${randSearch}&sca_esv=e84d3365b4113c52&ei=oHsuZ4u2OuaMnesP5sLToAI&ved=0ahUKEwiLhrOv0M2JAxVmRmcHHWbhFCQQ4dUDCA8&uact=5&oq=site%3Agoodreads.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFHNpdGU6Z29vZHJlYWRzLmNvbSBhSPYYUIUKWIwQcAV4AJABAJgBzQGgAfcFqgEFMC4yLjK4AQPIAQD4AQGYAgCgAgCYAwCIBgGSBwCgB7QB&sclient=gws-wiz-serp`
   }
   return (
     <div className='App' onClick={handleClick}>
