97706566673c485ae2681bf149aa99c438f6b7df
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-11-22 15:44:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * (2995 - 1 + 1) + 1 )]
     
-    window.location = `https://www.google.com/search?q=site%3Agoodreads.com+${randSearch}&sca_esv=e84d3365b4113c52&ei=oHsuZ4u2OuaMnesP5sLToAI&ved=0ahUKEwiLhrOv0M2JAxVmRmcHHWbhFCQQ4dUDCA8&uact=5&oq=site%3Agoodreads.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFHNpdGU6Z29vZHJlYWRzLmNvbSBhSPYYUIUKWIwQcAV4AJABAJgBzQGgAfcFqgEFMC4yLjK4AQPIAQD4AQGYAgCgAgCYAwCIBgGSBwCgB7QB&sclient=gws-wiz-serp`
+    window.location = 'https://www.google.com/search?q=site%3Agoodreads.com+${randSearch}+books&sca_esv=332c1457e26e21ac&ei=XVlAZ4D0I9WrseMP8IWxyAU&oq=site%3Agoodreads.com+a+books&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIhpzaXRlOmdvb2RyZWFkcy5jb20gYSBib29rc0jhFVCpFVipFXAAeACQAQCYAdYBoAHWAaoBAzItMbgBA8gBAPgBAZgCAKACAJgDAIgGAZIHAKAHLQ&sclient=mobile-gws-wiz-serp'
   }
   return (
     <div className='App' onClick={handleClick}>
