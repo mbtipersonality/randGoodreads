3f37baa75974ce636ff153f5a84dd01b841545aa
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-09-30 21:03:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     const search = localStorage.getItem('money')
     const randSearch = search[Math.floor(Math.random() * (search.length - 1 + 1) + 1 )]
 
-    const newMoney = search.filter(item => item !== randSearch))
+    const newMoney = search.filter(item => item!==randSearch))
     localStorage.setItem('money', JSON.stringify(newMoney))
       .then(window.location = `https://www.google.com/search?q=site%3Agoodreads.com+popular+${randSearch}+books&sca_esv=e84d3365b4113c52&ei=Ypr4Z-vhOPKf4-EPy5LsgAQ&ved=0ahUKEwjrmoCOks-MAxXyzzgGHUsJG0AQ4dUDCBA&uact=5&oq=site%3Agoodreads.com+popular+a+books&gs_lp=Egxnd3Mtd2l6LXNlcnAiInNpdGU6Z29vZHJlYWRzLmNvbSBwb3B1bGFyIGEgYm9va3NIgiZQsgJY1h9wAngAkAEAmAGcAqAB6xaqAQYwLjIuMTG4AQPIAQD4AQGYAgCgAgCYAwCIBgGSBwCgB8kEsgcAuAcA&sclient=gws-wiz-serp`)
   }
