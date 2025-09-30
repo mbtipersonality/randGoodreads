41d19cc083c14c77ee6216f4ec293314c5b038af
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-09-30 21:26:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css'
 import React from 'react'
-import search from './search'
+import search1 from './search'
 
 function App() {
   const handleClick = () => {
     // const randSearch = search[Math.floor(Math.random() * (1266 - 1 + 1) + 1 )]
-    const search = JSON.parse(localStorage.getItem('money'))
-    if(!search){
-      localStorage.setItem('money', JSON.stringify(search))
+    const search2 = JSON.parse(localStorage.getItem('money'))
+    if(!search2){
+      localStorage.setItem('money', JSON.stringify(search1))
     }
-    const randSearch = search[Math.floor(Math.random() * (search.length - 1 + 1) + 1 )]
+    const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
 
-    const newMoney = search.filter(item => item !== randSearch)
+    const newMoney = search2.filter(item => item !== randSearch)
     localStorage.setItem('money', JSON.stringify(newMoney))
     window.location = `https://www.google.com/search?q=site%3Agoodreads.com+popular+${randSearch}+books&sca_esv=e84d3365b4113c52&ei=Ypr4Z-vhOPKf4-EPy5LsgAQ&ved=0ahUKEwjrmoCOks-MAxXyzzgGHUsJG0AQ4dUDCBA&uact=5&oq=site%3Agoodreads.com+popular+a+books&gs_lp=Egxnd3Mtd2l6LXNlcnAiInNpdGU6Z29vZHJlYWRzLmNvbSBwb3B1bGFyIGEgYm9va3NIgiZQsgJY1h9wAngAkAEAmAGcAqAB6xaqAQYwLjIuMTG4AQPIAQD4AQGYAgCgAgCYAwCIBgGSBwCgB8kEsgcAuAcA&sclient=gws-wiz-serp`
   }
