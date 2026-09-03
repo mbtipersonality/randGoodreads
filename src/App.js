233782ca89112b0d549b4548d21cd80dc79c7912
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2026-09-03 22:21:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
     const newSelfhelp = search2.filter(item => item !== randSearch)
     localStorage.setItem('selfhelp', JSON.stringify(newSelfhelp))
-    window.location = `https://www.google.com/search?q=site%3Agoodreads.com+popular+${randSearch}+books&sca_esv=e84d3365b4113c52&ei=Ypr4Z-vhOPKf4-EPy5LsgAQ&ved=0ahUKEwjrmoCOks-MAxXyzzgGHUsJG0AQ4dUDCBA&uact=5&oq=site%3Agoodreads.com+popular+a+books&gs_lp=Egxnd3Mtd2l6LXNlcnAiInNpdGU6Z29vZHJlYWRzLmNvbSBwb3B1bGFyIGEgYm9va3NIgiZQsgJY1h9wAngAkAEAmAGcAqAB6xaqAQYwLjIuMTG4AQPIAQD4AQGYAgCgAgCYAwCIBgGSBwCgB8kEsgcAuAcA&sclient=gws-wiz-serp`
+    window.location = `https://www.goodreads.com/list/tag?id=${randSearch}`
   }
   return (
     <div className='App' onClick={handleClick}>
