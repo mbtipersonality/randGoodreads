c182b777022f1428391a80470b2568b8d25f29c7
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2026-09-20 02:35:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       const randomNumber = listfromLocal[Math.floor(Math.random() * listfromLocal.length)];
       const newList = listfromLocal.filter(item => item !== randomNumber)
       localStorage.setItem('list', JSON.stringify(newList))
-      window.location = `https://www.goodreads.com/list/book/${randSearch}`
+      window.location = `https://www.goodreads.com/list/book/${randomNumber}`
     }
     else {
       const list = Array.from({ length: 9999997 }, (_, i) => i + 1);
