9ae276601b29da93d039777e1a9720b8ec7578ef
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2026-09-20 03:33:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,17 +16,27 @@ function App() {
     // localStorage.setItem('selfhelp', JSON.stringify(newSelfhelp))
     // window.location = `https://www.goodreads.com/list/tag?id=${randSearch}`
 
-    const listfromLocal = JSON.parse(localStorage.getItem('list'))
-    if (listfromLocal){
-      const randomNumber = listfromLocal[Math.floor(Math.random() * listfromLocal.length)];
-      const newList = listfromLocal.filter(item => item !== randomNumber)
-      localStorage.setItem('list', JSON.stringify(newList))
-      window.location = `https://www.goodreads.com/list/book/${randomNumber}`
-    }
-    else {
-      // const list = Array.from({ length: 9999997 }, (_, i) => i + 1);
-      localStorage.setItem('list', JSON.stringify(5))
-    } 
+    // const listfromLocal = JSON.parse(localStorage.getItem('list'))
+    // if (listfromLocal){
+    //   const randomNumber = listfromLocal[Math.floor(Math.random() * listfromLocal.length)];
+    //   const newList = listfromLocal.filter(item => item !== randomNumber)
+    //   localStorage.setItem('list', JSON.stringify(newList))
+    //   window.location = `https://www.goodreads.com/list/book/${randomNumber}`
+    // }
+    // else {
+    //   // const list = Array.from({ length: 9999997 }, (_, i) => i + 1);
+    //   localStorage.setItem('list', JSON.stringify(5))
+    // } 
+
+  function generateNumber() {
+  let digits = '';
+  for (let i = 0; i < 7; i++) {
+    digits += Math.floor(Math.random() * 10); // random digit 0-9
+  }
+  // Remove leading zeros
+  let result = parseInt(digits, 10); // parseInt handles leading zeros automatically
+  window.location = `https://www.goodreads.com/list/book/${result}`
+}
   }
   return (
     <div className='App' onClick={handleClick}>
