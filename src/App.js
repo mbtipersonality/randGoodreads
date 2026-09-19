606678d46009389a32d5fc5ec4eab6c3a9cac3ad
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2026-09-20 02:41:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     else {
       const list = Array.from({ length: 9999997 }, (_, i) => i + 1);
       console.log("here")
-      localStorage.setItem('list', JSON.stringify(list))
+      localStorage.setItem('list', JSON.stringify(5))
     } 
   }
   return (
