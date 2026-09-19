f7666335c2a3357ad995206dc7c81b030fe246e1
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2026-09-20 03:45:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,37 +5,18 @@ import React from 'react'
 function App() {
   const handleClick = () => {
     // const randSearch = search[Math.floor(Math.random() * (1266 - 1 + 1) + 1 )]
-    // const search2 = JSON.parse(localStorage.getItem('selfhelp'))
-    // if(!search2){
-    //   localStorage.setItem('selfhelp', JSON.stringify(search1))
-    //   return
-    // }
-    // const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
-
-    // const newSelfhelp = search2.filter(item => item !== randSearch)
-    // localStorage.setItem('selfhelp', JSON.stringify(newSelfhelp))
-    // window.location = `https://www.goodreads.com/list/tag?id=${randSearch}`
-
-    // const listfromLocal = JSON.parse(localStorage.getItem('list'))
-    // if (listfromLocal){
-    //   const randomNumber = listfromLocal[Math.floor(Math.random() * listfromLocal.length)];
-    //   const newList = listfromLocal.filter(item => item !== randomNumber)
-    //   localStorage.setItem('list', JSON.stringify(newList))
-    //   window.location = `https://www.goodreads.com/list/book/${randomNumber}`
-    // }
-    // else {
-    //   // const list = Array.from({ length: 9999997 }, (_, i) => i + 1);
-    //   localStorage.setItem('list', JSON.stringify(5))
-    // } 
-
-  
-  let digits = '';
-  for (let i = 0; i < 7; i++) {
-    digits += Math.floor(Math.random() * 10); // random digit 0-9
-  }
-  // Remove leading zeros
-  let result = parseInt(digits, 10); // parseInt handles leading zeros automatically
-  window.location = `https://www.goodreads.com/list/book/${result}`
+   
+    const listfromLocal = JSON.parse(localStorage.getItem('list'))
+    if (listfromLocal){
+      const randomNumber = listfromLocal[Math.floor(Math.random() * listfromLocal.length)];
+      const newList = listfromLocal.filter(item => item !== randomNumber)
+      localStorage.setItem('list', JSON.stringify(newList))
+      window.location = `https://www.goodreads.com/list/book/${randomNumber}`
+    }
+    else {
+      const list = Array.from({ length: 10000 }, (_, i) => i + 1);
+      localStorage.setItem('list', JSON.stringify(list))
+    } 
   }
   return (
     <div className='App' onClick={handleClick}>
